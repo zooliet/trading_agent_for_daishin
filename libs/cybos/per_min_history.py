814ab623753fb650c3e5df9c41f3a0584372ba6e
Junhyun Shin <hl1sqi@gmail.com>
2020-08-11 13:07:13 +0900

    change mqtt payload for 1m download

--- libs/cybos/per_min_history.py
+++ libs/cybos/per_min_history.py
@@ -8,6 +8,8 @@ if sys.platform == 'win32':
     import pythoncom
 
 from datetime import datetime
+import time
+import asyncio
 
 class PerMinHistory:
     def __init__(self, mqtt, logger=None):
@@ -19,7 +21,7 @@ class PerMinHistory:
         self.mqtt = mqtt
 
     async def request(self, asset):
-        num_requested = 100 # upto 200000
+        num_requested = 200 # upto 200000 (하루 약 400개=> 200000/400 = 500일)
         client = win32com.client.Dispatch("CpSysDib.StockChart")
         client.SetInputValue(0, asset)
         client.SetInputValue(1, ord('2')) # 갯수로 받기
@@ -33,26 +35,29 @@ class PerMinHistory:
         while received_total < num_requested: 
             client.BlockRequest()
             if client.GetDibStatus() == 0:  # ready to send
-                message = {
-                    'action': 'per_min_history'
-                }
+                message = {'action': 'per_min_history', 'code': asset}
                 count = client.GetHeaderValue(3)  # 수신 개수
                 # code = client.GetHeaderValue(0)  #종목코드
                 # name= client.GetHeaderValue(1)  # 종목명
                 for i in range(count):
                     received_total += 1
+                    if received_total > num_requested:
+                        break
                     date=client.GetDataValue(0, i)
                     time=client.GetDataValue(1, i)
                     open=client.GetDataValue(2, i)
                     close=client.GetDataValue(5, i)
                     vol=client.GetDataValue(6, i)
-                    self.logger.debug(f"[{received_total}/{num_requested}] {date} {time}: {open}(o), {close}(c), {vol}(v)")
+                    # self.logger.debug(f"[{received_total}/{num_requested}] {date} {time}: {open}(o), {close}(c), {vol}(v)")
                     # date_time = datetime.strptime(f'{date} {time:04d}', '%Y%m%d %H%M')
                     date_time = f'{date} {time:04d}'
-                    message[date_time] = { 'code': asset, 'close': close, 'volume': vol }
+                    # message[date_time] = { 'current': close, 'volume': vol }
+                    message[date_time] = (close, vol)
 
+                self.logger.debug(f"[{received_total}/{num_requested}]")
                 message = bytearray(json.dumps(message), 'utf-8')
-                # await self.mqtt.publish('rekcle/cybos/response', message, qos=0x00)
+                await self.mqtt.publish('rekcle/cybos/response', message, qos=0x00)
+                await asyncio.sleep(0.25)
             else:
                 status_message = client.GetDibMsg1()
                 self.logger.info(f'통신오류: {status_message}')
